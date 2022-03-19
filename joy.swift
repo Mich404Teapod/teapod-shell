91b0d9274b9e5e4670be5c9a41b0ec0bc5f04594
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-03-19 12:29:13 +0100

    Update joy.swift

--- joy.swift
+++ joy.swift
@@ -9,31 +9,4 @@ func main(_ argc: Int, _ argv: [Int8]?) -> Int {
 
 func plane(from anchor: ARPlaneAnchor?) -> SCNNode? {
     let plane = SCNPlane(width: CGFloat(anchor?.extent.x ?? 0.0), height: CGFloat(anchor?.extent.z ?? 0.0))
-   
-   
-   //error function
-func error {
-public init(device: IOHIDDevice) {
-        self.device = device
-        self.serialID = IOHIDDeviceGetProperty(device, kIOHIDSerialNumberKey as CFString) as? String ?? ""
-        self.handlers = [:]
-        self.spiReadHandler = [:]
-        self.isConnected = false
-        self.packetCounter = 0
-        self.rumbleData = [0x00, 0x01, 0x00, 0x40, 0x00, 0x01, 0x00, 0x40]
-        self.subcommandQueue = []
-        self.battery = .unknown
-        self.isCharging = false
-        self.buttonState = [:]
-        self.leftStickDirection = .Neutral
-        self.rightStickDirection = .Neutral
-        self.lStickRawPos = CGPoint(x: 0, y: 0)
-        self.lStickPos = CGPoint(x: 0, y: 0)
-        self.rStickRawPos = CGPoint(x: 0, y: 0)
-        self.rStickPos = CGPoint(x: 0, y: 0)
-        self.acceleration = SCNVector3(x: 0, y: 0, z: 0)
-        self.gyro = SCNVector3(x: 0, y: 0, z: 0)
-        self.bodyColor = CGColor(red: 0.333, green: 0.333, blue: 0.333, alpha: 0.333)
-        self.buttonColor = CGColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-   }
-}
+    
