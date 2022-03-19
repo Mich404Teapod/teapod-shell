b4dd40205220b4a5e7bf9fecac8288a2063ef778
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-03-19 12:02:33 +0100

    Update joy.swift

--- joy.swift
+++ joy.swift
@@ -1,4 +1,19 @@
-   public init(device: IOHIDDevice) {
+import UIKit
+
+func main(_ argc: Int, _ argv: [Int8]?) -> Int {
+    autoreleasepool {
+        return Int(UIApplicationMain(Int32(argc), &argv, nil, NSStringFromClass(AppDelegate.self.self)))
+    }
+}
+
+
+func plane(from anchor: ARPlaneAnchor?) -> SCNNode? {
+    let plane = SCNPlane(width: CGFloat(anchor?.extent.x ?? 0.0), height: CGFloat(anchor?.extent.z ?? 0.0))
+   
+   
+   //error function
+func error {
+public init(device: IOHIDDevice) {
         self.device = device
         self.serialID = IOHIDDeviceGetProperty(device, kIOHIDSerialNumberKey as CFString) as? String ?? ""
         self.handlers = [:]
@@ -20,18 +35,5 @@
         self.gyro = SCNVector3(x: 0, y: 0, z: 0)
         self.bodyColor = CGColor(red: 0.333, green: 0.333, blue: 0.333, alpha: 0.333)
         self.buttonColor = CGColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    }
-    
-    func readInitializeData(_ done: @escaping () -> Void) {
-        self.readControllerColor {
-            self.readCalibration()
-            // TODO: Call done() after readCalibration() is done
-            done()
-        }
-    }
-    
-    func handleError(result: Int32, value: IOHIDValue) {}
-
-    func handleInput(value: IOHIDValue) {
-        let element = IOHIDValueGetElement(value)
-        let reportID = IOHIDElementGetReportID(element)
+   }
+}
